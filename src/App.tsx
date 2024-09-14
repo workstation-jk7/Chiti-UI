3f36be673032e2f5945ba83dca372dc75c2adc73
JK <workstation.jk7@gmail.com>
2024-09-14 15:23:52 +0530

    added new images

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
 import './App.css'
 
-import { useState, useEffect } from 'react';
-
-function App() {
-  const [deferredPrompt, setDeferredPrompt] = useState(null);
-  const [showInstallButton, setShowInstallButton] = useState(false);
-
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (event: any) => {
-      event.preventDefault();
-      setDeferredPrompt(event);
-      setShowInstallButton(true);
-    };
-
-    const isInstalled = window.matchMedia('(display-mode: standalone)').matches;
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
-
-    if (!isInstalled && !isIOS) {
-      window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    }
-
-    return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    };
-  }, []);
-
-  const handleInstallClick = () => {
-    if (deferredPrompt) {
-      (deferredPrompt as any).prompt();
-      (deferredPrompt as any).userChoice.then((choiceResult: any) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-        setDeferredPrompt(null);
-        setShowInstallButton(false);
-      });
-    }
-  };
-
+const App = () => {
+  
   return (
     <div className="welcome-container">
       <img src="../images/chiti-vertical-logo.svg"/>
       {/* Your app content */}
-      {/* {showInstallButton && (
-        <button onClick={handleInstallClick} id="install-button">
-          Install App
-        </button>
-      )} */}
     </div>
   );
 }
