0eee154bb1f5ff0ec01da59484fbec1ab6064ad4
JK <workstation.jk7@gmail.com>
2024-09-15 15:41:17 +0530

    added global color.css

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,6 +10,7 @@ const LoginPage: React.FC = () => {
   const navigate = useNavigate();
   const handleLogin = async () => {
     try {
+      await auth.signOut();
       const result = await signInWithPopup(auth, provider);
       // The signed-in user info
       const user = result.user;
@@ -23,25 +24,14 @@ const LoginPage: React.FC = () => {
       console.error('Error during sign-in:', error);
     }
   };
-  const handleSignOut = async () => {
-    try {
-      await auth.signOut();
-      console.log("User signed out");
-      // Optionally redirect the user or show a message
-      // window.location.href = "/login"; // Example redirection
-    } catch (error) {
-      console.error("Error signing out: ", error);
-    }
-  };
   return (
     <div className='login-container'>
-      <button onClick={handleSignOut}>Sign Out</button>
       <img src='/images/chiti-horizontal-logo.svg'/>
       <img src='/images/ruppee-background.svg'/>
       <div className='sign-in-control'>
         <button onClick={handleLogin}>
             <img className='google-icon' src='/images/google-logo.svg'/>
-            Login with Google
+            Sign in with Google
         </button>
       </div>
     </div>
